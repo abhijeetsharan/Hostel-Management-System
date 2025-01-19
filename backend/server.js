b78ed9a74a4dcdcdb9cc97bccbd9028d98d64f13
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-01-20 02:40:43 +0530

    Updated User Model

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
-//Load Environment Variables
-dotenv.config();
+const app = express();
 
 //Connect to the Database
 connectDB();
 
-const app = express();
-
 //Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true}))
 
-//Routes
-app.use('/api/auth', require('./routes/auth'));
+//API Endpoints
+app.get('/', (req, res) => {
+    res.send('API Working')
+})
 
 //Server setup
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
